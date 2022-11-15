d35d3def870cae69ec07bf3ca3f2befc7cc7f24e
irisu01 <nekomarisakirisame@gmail.com>
2022-11-15 08:30:44 -0500

    flatline the proposal until i freely start

--- src/standard.rs
+++ src/standard.rs
@@ -9,12 +9,7 @@ pub struct StandardPointCalculator {
 }
 
 impl PointCalculator for StandardPointCalculator {
-    // 250 * EXP(LN(250 / 5) / (1 - list_size) * (demon - 1))
-    // 250f64 * f64::exp(f64::ln(250f64 / 15f64) / (1f64 - 75f64) * (f64::from(self.base.position) - 1f64));
     fn calculate(&self) -> f64 {
-        let top_value: f64 = 250.0;
-        let low_value: f64 = 5.0;
-        
-        250.0f64 * f64::exp(f64::ln(top_value / low_value) / (1.0f64 - self.list_size as f64) * (f64::from(self.position) - 1.0f64))
+        0.0
     }
 }
