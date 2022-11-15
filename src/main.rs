1e2694d302bd399912f3e4f5a3ab63056248432e
Irisu <nekomarisakirisame@gmail.com>
2022-11-15 09:04:08 -0500

    allow for calculating just the points

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,18 @@ fn main() {
             process::exit(1);
         }
     }
+    
+    // Accept an optional argument.
+    let mut just_points: bool = false;
+    if args.len() > 3 {
+        match args[4].parse::<bool>() {
+            Ok(tf) => just_points = tf,
+            Err(_) => {
+                println!("Error: The fourth argument must be a boolean. (true/false0");
+                process::exit(1);
+            }
+        }
+    }
 
     // Create the calculator.
     let calculator = standard::StandardPointCalculator {
@@ -59,6 +71,10 @@ fn main() {
     let points = calculator.calculate();
 
     // Print the points.
-    println!("Points Worth: {}", points);
-
+    if just_points == true {
+       println!("{}", points); 
+    } else {
+        println!("Calculating points for position {} with {} victors in a list of {}.", calculator.position, calculator.victor_count, calculator.list_size);
+        println!("Points Worth: {}", points);
+    }
 }
